b46b4ea70d1d5190046cba46764b6475f7c9cda5
Austine The-Enigma <austine574@gmail.com>
2026-02-04 18:27:27 +0100

    removed unnecessary files in gitignore

--- hello_cargo/target/debug/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-C:\Users\USER\rusty\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
-
-C:\Users\USER\rusty\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
-
-src\main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-C:\Users\USER\rusty\hello_cargo\target\debug\hello_cargo.exe: C:\Users\USER\rusty\hello_cargo\src\main.rs
