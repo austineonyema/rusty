3cd7c12b61bc183d4a11fb921fb26bb63d3a3533
Austine The-Enigma <austine574@gmail.com>
2026-02-04 01:21:21 +0100

    intro to rust cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\USER\rusty\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+C:\Users\USER\rusty\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\USER\rusty\hello_cargo\target\debug\hello_cargo.exe: C:\Users\USER\rusty\hello_cargo\src\main.rs
